4184f7a1c725c2a66ec851c072bf5f0d4ab422c0
tobeannouncd <wzrds3@gmail.com>
2017-11-30 10:42:02 -0500

    Added problems 37-38

---
+++ p037.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 08:31:03 2017
+
+@author: Tyler
+
+Project Euler problem 37: truncatable primes
+
+The number 3797 has an interesting property. Being prime itself, it is possible
+to continuously remove digits from left to right, and remain prime at each 
+stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 
+379, 37, and 3.
+
+Find the sum of the only eleven primes that are both truncatable from left to 
+right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+"""
+from factor_functions import is_prime
+
+def is_trunc_prime(num):
+    '''
+    num: number to check
+    
+    output: true if num is truncatable prime, false otherwise
+    '''
+    for i in range(len(str(num)) - 1):
+        if not is_prime(int(''.join(list(str(num))[:i+1]))): return False
+        if not is_prime(int(''.join(list(str(num))[len(str(num))-i-1:]))):
+            return False
+    return True
+
+def next_candidate(num):
+    '''
+    num: truncatable prime candidate
+    
+    output: prime number that might be truncatable
+    '''
+    while True:
+        if num % 10 == 3: num += 4
+        elif num % 10 == 7:
+            num += 6
+            num_list = list(str(num))
+            first_digit = int(num_list[0])
+            if first_digit in (1, 4, 6):
+                num_list[0] = str(int(num_list[0]) + 1)
+            elif first_digit > 7:
+                num_list[0] = '20'
+            num = int(''.join(num_list))
+        if is_prime(num): break
+    return num
+
+def main():
+    count = 0
+    n = 23
+    s = 0
+    while count < 11:
+        if is_trunc_prime(n):
+            count += 1
+            s += n
+        n = next_candidate(n)
+    print(s)
+    pass
+
+if __name__ == '__main__': main()
---
+++ p038.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 09:47:58 2017
+
+@author: Tyler
+
+Project Euler problem 38: pandigital multiples
+
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 × 1 = 192
+192 × 2 = 384
+192 × 3 = 576
+
+By concatenating each product we get the 1 to 9 pandigital, 192384576. We will 
+call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 
+5, giving the pandigital, 918273645, which is the concatenated product of 9 and
+(1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the 
+concatenated product of an integer with (1,2, ... , n) where n > 1?
+"""
+def concat_products(num):
+    '''
+    num: number to multiply
+    
+    output: result of multiplying num by 1, 2, ..., n and concatenating to 
+    prior products until 9 digit number is formed
+    '''
+    out = ''
+    i = 1
+    while len(out) < 9:
+        out += str(num * i)
+        i += 1
+    return int(out)
+
+def is_pandigital(num):
+    '''
+    num: number to check
+    
+    output: True if number is pandigital (1-9), False otherwise
+    '''
+    num = str(num)
+    if len(num) != 9: return False
+    for i in range(1, len(num)):
+        if num[i] in num[:i] or num[i] == '0': return False
+    return True
+
+def main():
+    c = []
+    for i in range(1,10**6):
+        if is_pandigital(concat_products(i)): c.append((i, concat_products(i)))
+    print(c)
+    pass
+
+if __name__ == '__main__': main()
