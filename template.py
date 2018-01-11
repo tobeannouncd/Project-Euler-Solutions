cc18e96d02722e6722ce6f667b2190f7c7065082
tobeannouncd <wzrds3@gmail.com>
2018-01-11 15:10:26 -0500

    updated template

--- template.py
+++ template.py
@@ -8,9 +8,7 @@ from time import clock
 
 start_time = clock()
 
-# =============================================================================
 # INSERT CODE HERE
-# =============================================================================
 
 time_elapsed = clock() - start_time
 print('Executed in {:.3G} seconds.'.format(time_elapsed))
